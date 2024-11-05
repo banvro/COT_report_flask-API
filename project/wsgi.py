0c1e25381441dc1f2d87340ae690c7002bf35d48
Naresh <banvro07@gmail.com>
2024-11-05 12:20:02 +0530

    fix ap

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,4 +1,7 @@
-import main
+from main import app  # Make sure to import your Flask app correctly
+
+# The Gunicorn server expects an 'application' callable by default.
+application = app
 
 if __name__ == "__main__":
-    main.run()
+    application.run()
